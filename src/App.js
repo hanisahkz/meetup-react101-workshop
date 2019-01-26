6aa8b03b58252896d09ed876425b547bb0ea9ee5
Nurul Zaman <nurul.zaman@servicerocket.com>
2019-01-26 13:59:35 +0800

    Buggy commit. Need to be fixed.

--- src/App.js
+++ src/App.js
@@ -1,26 +1,60 @@
 import React from 'react';
 import Movie from './Movie';
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      showMovies: false
+    };
+    this.showMovies = this.showMovies.bind(this);
+  }
 
-const App = function() {
-  return React.createElement('div', {}, [
-    React.createElement(
-      'div',
-      { className: 'title-bar' },
-      React.createElement('h1', {}, 'React Movie App')
-    ),
-    React.createElement(Movie, {
-      title: 'Title 1',
-      date: Math.random(10)
-    }),
-    React.createElement(Movie, {
-      title: 'Title 2',
-      date: Math.random(10)
-    }),
-    React.createElement(Movie, {
-      title: 'Title 3',
-      date: Math.random(10)
-    })
-  ]);
-};
+  showMovies() {
+    this.setState({
+      showMovies: true
+    });
+  }
+
+  render() {
+    let movies;
+
+    if (this.state.showMovies) {
+      movies = (
+        // <React.Fragment>
+        //   <Movie name="Aquaman" releaseDate="2018-12-07" />
+        //   <Movie name="Bumblebee" releaseDate="2018-12-15" />
+        //   <Movie
+        //     name="Fantastic Beasts: The Crimes of Grindelwald"
+        //     releaseDate="2018-11-14"
+        //   />
+        // </React.Fragment>
+
+        // is equivalent to this:
+        <>
+          <Movie name="Aquaman" releaseDate="2018-12-07" />
+          <Movie name="Bumblebee" releaseDate="2018-12-15" />
+          <Movie
+            name="Fantastic Beasts: The Crimes of Grindelwald"
+            releaseDate="2018-11-14"
+          />
+        </>
+      );
+    }
+
+    return (
+      <div>
+        <div className="title-bar">
+          <h1>React Movie App</h1>
+        </div>
+        <div className="button-container">
+          <button onClick={this.showMovies} className="button">
+            Show Movies
+          </button>
+        </div>
+        {movies}
+      </div>
+    );
+  }
+}
 
 export default App;
--- src/Movie.js
+++ src/Movie.js
@@ -1,9 +1,10 @@
 import React from 'react';
 
-const Movie = props =>
-  React.createElement('div', { className: 'movie-container' }, [
-    React.createElement('h1', {}, props.title),
-    React.createElement('span', {}, props.date)
-  ]);
+const Movie = props => (
+  <div className="movie-container">
+    <h1>{props.name}</h1>
+    <h2>{props.releaseDate}</h2>
+  </div>
+);
 
 export default Movie;
