8042188f30b678cbe53ee4822abb5b32460b1a8d
Nurul Zaman <nurul.zaman@servicerocket.com>
2019-01-26 11:08:27 +0800

    Implement eslint for linting

--- script.js
+++ script.js
@@ -1,9 +1,3 @@
-// Movie component that has div > h2, h1
-//   const Movie = () =>
-//     React.createElement("div", { className: "movie-container" }, [
-//       React.createElement("h1", {}, "Aquaman"),
-//       React.createElement("span", {}, "2018-12-07")
-//     ]);
 
 const Movie = props =>
   React.createElement('div', { className: 'movie-container' }, [
@@ -12,9 +6,6 @@ const Movie = props =>
   ]);
 
 const App = function() {
-  //   App component needs to have Movie component inside
-  // So, pass the Movie component in a form of array
-  // this is how the component Movie will be rendered
   return React.createElement('div', {}, [
     React.createElement(
       'div',
@@ -35,15 +26,5 @@ const App = function() {
     })
   ]);
 };
-
-//   const App = function() {
-//     //   React.createelement 1st params - the html tag
-//     // 2nd tag can be like {id:'HTMLID'}
-//     return React.createElement(
-//       "div",
-//       {},
-//       React.createElement("h1", {}, "React Movie  App")
-//     );
-//   };
 
 ReactDOM.render(React.createElement(App), document.getElementById('root'));
