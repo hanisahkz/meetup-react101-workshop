91992959f957d6145c14b3cc72355112e53c7a9c
Nurul Zaman <nurul.zaman@servicerocket.com>
2019-01-26 14:19:20 +0800

    Fix code

--- src/App.js
+++ src/App.js
@@ -1,57 +1,37 @@
 import React from 'react';
 import Movie from './Movie';
-class App extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      showMovies: false
-    };
-    this.showMovies = this.showMovies.bind(this);
-  }
 
-  showMovies() {
-    this.setState({
-      showMovies: true
-    });
-  }
+class App extends React.Component {
+  state = {
+    showMovies: false
+  };
+
+  toggleMovies = () =>
+    this.setState(prevState => ({
+      showMovies: !prevState.showMovies
+    }));
 
   render() {
-    let movies;
-
-    if (this.state.showMovies) {
-      movies = (
-        // <React.Fragment>
-        //   <Movie name="Aquaman" releaseDate="2018-12-07" />
-        //   <Movie name="Bumblebee" releaseDate="2018-12-15" />
-        //   <Movie
-        //     name="Fantastic Beasts: The Crimes of Grindelwald"
-        //     releaseDate="2018-11-14"
-        //   />
-        // </React.Fragment>
-
-        // is equivalent to this:
-        <>
-          <Movie name="Aquaman" releaseDate="2018-12-07" />
-          <Movie name="Bumblebee" releaseDate="2018-12-15" />
-          <Movie
-            name="Fantastic Beasts: The Crimes of Grindelwald"
-            releaseDate="2018-11-14"
-          />
-        </>
-      );
-    }
-
     return (
       <div>
         <div className="title-bar">
           <h1>React Movie App</h1>
         </div>
         <div className="button-container">
-          <button onClick={this.showMovies} className="button">
-            Show Movies
+          <button onClick={this.toggleMovies} className="button">
+            {this.state.showMovies ? 'Hide' : 'Show'} Movies
           </button>
         </div>
-        {movies}
+        {this.state.showMovies && (
+          <>
+            <Movie name="Aquaman" releaseDate="2018-12-07" />
+            <Movie name="Bumblebee" releaseDate="2018-12-15" />
+            <Movie
+              name="Fantastic Beasts: The Crimes of Grindelwald"
+              releaseDate="2018-11-14"
+            />
+          </>
+        )}
       </div>
     );
   }
